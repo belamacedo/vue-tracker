279f5ebf91f1468984bf1d36da1d91fd04f7e3e2
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-06-18 16:11:01 -0300

    Cronometro component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -16,9 +16,7 @@
 				<div
 					class="is-flex is-align-items-center is-justify-content-space-between"
 				>
-					<section>
-						<strong>{{ tempoDecorrido }}</strong>
-					</section>
+					<Cronometro :tempoEmSegundos="tempoEmSegundos" />
 					<button class="button" @click="iniciar">
 						<span class="icon">
 							<i class="fas fa-play"></i>
@@ -39,20 +37,17 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
 
 export default defineComponent({
 	// eslint-disable-next-line vue/multi-word-component-names
 	name: 'Formulário',
+	components: { Cronometro },
 	data() {
 		return {
 			tempoEmSegundos: 0,
 			cronometro: 0,
 		};
-	},
-	computed: {
-		tempoDecorrido(): string {
-			return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
-		},
 	},
 	methods: {
 		iniciar() {
