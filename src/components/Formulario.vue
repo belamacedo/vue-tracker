e6f729a1df28e93a194d6541d1533e9db26d3685
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-06-18 15:49:59 -0300

    cronometrando tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,15 +17,15 @@
 					class="is-flex is-align-items-center is-justify-content-space-between"
 				>
 					<section>
-						<strong>00:00:00</strong>
+						<strong>{{ tempoDecorrido }}</strong>
 					</section>
-					<button class="button">
+					<button class="button" @click="iniciar">
 						<span class="icon">
 							<i class="fas fa-play"></i>
 						</span>
 						<span>play</span>
 					</button>
-					<button class="button">
+					<button class="button" @click="finalizar">
 						<span class="icon">
 							<i class="fas fa-stop"></i>
 						</span>
@@ -43,5 +43,28 @@ import { defineComponent } from 'vue';
 export default defineComponent({
 	// eslint-disable-next-line vue/multi-word-component-names
 	name: 'Formulário',
+	data() {
+		return {
+			tempoEmSegundos: 0,
+		};
+	},
+	computed: {
+		tempoDecorrido(): string {
+			return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+		},
+	},
+	methods: {
+		iniciar() {
+			// começar a contagem
+			// 1 seg = 1000 ms
+			setInterval(() => {
+				this.tempoEmSegundos += 1;
+			}, 1000);
+			console.log('iniciando');
+		},
+		finalizar() {
+			console.log('finalizando');
+		},
+	},
 });
 </script>
