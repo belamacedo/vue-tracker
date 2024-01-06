bde9dc4c60144b17aaee0be6d5636ad774edb8d7
Isabela Macedo <isabelacb.macedo@gmail.com>
2024-01-06 13:24:02 -0300

    feat: added mutations

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,17 +1,19 @@
 <template>
-	<div class="box has-text-weight-bold">
-		<slot></slot>
-	</div>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-// eslint-disable-next-line vue/multi-word-component-names
-export default defineComponent({ name: 'Box' });
+
+export default defineComponent({
+  name: 'Box'
+});
 </script>
 
 <style scoped>
 .box {
-	background: #faf0ca;
+  background: #FAF0CA;
 }
 </style>
